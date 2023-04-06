51523e1109d84e72afc458b76b455a84d49c4751
hasibimamhridoy <hasibimamhridoy@gmail.com>
2023-04-06 09:50:16 +0600

    bug fixing

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -43,7 +43,7 @@ const Product = ({ product, handleCartItem, handleFavoriteItem }) => {
   return (
     <div>
       <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-        <a href="#">
+        <a >
           <img
             className=" w-full h-52 rounded-t-lg object-fill p-5 rounded-lg z-5"
             src={images[0]}
@@ -52,7 +52,7 @@ const Product = ({ product, handleCartItem, handleFavoriteItem }) => {
         </a>
         <div className="px-5 pb-5">
           <div className="flex justify-between">
-          <a href="#">
+          <a >
             <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
               {title}
             </h5>
